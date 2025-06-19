01b98efa58e5900ef1f9aa429613bc4d7cb4de32
Akshaay Kaldev <163286127+AkshaayDev@users.noreply.github.com>
2025-06-19 16:24:26 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,49 +13,70 @@ std::vector<std::pair<double, double>> points = {
 	{250, 8.5},
 	{300, 9.8},
 };
+double N = points.size();
 
-// Approximate the dataset with y = mx + c
+// Model the dataset with y = mx + c
 double m = 0; // Gradient
 double c = 0; // y-Intercept
 double learningRate = 0.00000001;
 int iterations = 10000000;
 
-double MSE(double m, double c) { // Mean Squared Error
-	double sum = 0;
-	for (int i = 0; i < points.size(); i++) {
-		double x = points[i].first;
-		double y = points[i].second;
-		sum += std::pow((m * x + c) - y, 2);
-	}
-	return sum / points.size();
+// Predict the y-value for a given x-value using the parameters
+double predict(double x) {
+	return m * x + c;
 }
 
-int main() {
-	// Gradient descent
+// Mean Squared Error
+double MSE() {
+	double sum = 0;
+	for (const auto& [X, Y] : points) {
+		sum += std::pow(predict(X) - Y, 2);
+	}
+	return sum / N;
+}
+
+// Gradient descent to approximate the slope and y-intercept
+void gradientDescent() {
 	for (int i = 0; i < iterations; i++) {
 		double dm = 0; // Partial derivative of MSE with respect to slope(m)
 		double dc = 0; // Partial derivative of MSE with respect to intercept(c)
 
-		for (int j = 0; j < points.size(); j++) {
-			double x = points[j].first;
-			double y = points[j].second;
-			double error = (m * x + c) - y;
-			dm += 2 * error * x;
+		for (const auto& [X, Y] : points) {
+			double error = predict(X) - Y;
+			dm += 2 * error * X;
 			dc += 2 * error;
 		}
-		dm /= points.size();
-		dc /= points.size();
+		dm /= N;
+		dc /= N;
 		m -= learningRate * dm;
 		c -= learningRate * dc;
 	}
+}
 
-	std::cout << "Points: ";
-	for (int i = 0; i < points.size(); i++) {
-		std::cout << "(" << points[i].first << "," << points[i].second << ")";
-		if (i != points.size() - 1) { std::cout << ","; }
-		else { std::cout << std::endl; }
+// Calculate the expected slope and y-intercept
+void linearRegression() {
+	double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
+	for (const auto& [X, Y] : points) {
+		sumX += X; sumY += Y; sumXY += X*Y; sumX2 += X*X;
 	}
-	std::cout << "Slope = " << m << std::endl;
-	std::cout << "y-Intercept = " << c << std::endl;
-	std::cout << "Mean Squared Error = " << MSE(m, c) << std::endl;
+	m = (N * sumXY - sumX * sumY) / (N * sumX2 - sumX * sumX);
+	c = (sumY - m * sumX) / N;
+}
+
+int main() {
+	std::cout << "Points: ";
+	for (int i = 0; i < N; i++) {
+		std::cout << "(" << points[i].first << "," << points[i].second << ")";
+		if (i < N-1) { std::cout << ","; }
+	}
+	std::cout << "\n\n";
+
+	for (int i = 0; i < 2; i++) {
+		(i == 0) ? gradientDescent() : linearRegression();
+		std::cout << ((i == 0) ? "Predicted" : "Expected") << ":\n";
+
+		std::cout << "Slope = " << m << '\n';
+		std::cout << "y-Intercept = " << c << '\n';
+		std::cout << "MSE = " << MSE() << "\n\n";
+	}
 }
